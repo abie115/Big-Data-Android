cdcd2d7d24b09f26ccb75ebef4ef76ff70cc1287
nkopa <natalia16k@wp.pl>
2015-11-13 23:52:51 +0100

    #3 Ustawienie pola dla zmiany czestotliwosci

--- StepsHunter/app/src/main/java/com/android/bigdata/stepshunter/HunterService.java
+++ StepsHunter/app/src/main/java/com/android/bigdata/stepshunter/HunterService.java
@@ -4,6 +4,12 @@ package com.android.bigdata.stepshunter;
  * Created by Natalia on 2015-11-12.
  */
 public class HunterService {
+
+    private final long DEFAULT_FREQUENCY = 30;
+    private final long MIN_FREQUENCY = 30;
+    private final long MAX_FREQUENCY = 30;
+    private long CURRENT_FREQUENCY = DEFAULT_FREQUENCY;
+
     private static HunterService ourInstance = new HunterService();
 
     public static HunterService getInstance() {
