ea3678324d91a7f9bbed09521c0f13a22e369fcb
Agnieszka <agnieszka-garsta@wp.pl>
2015-11-19 22:07:59 +0100

    prosta notyfikacje informujaca o straconym sygnale gps, po kliknieciu przywracajaca main activity #2

--- StepsHunter/app/src/main/java/com/android/bigdata/stepshunter/HunterService.java
+++ StepsHunter/app/src/main/java/com/android/bigdata/stepshunter/HunterService.java
@@ -1,9 +1,18 @@
 package com.android.bigdata.stepshunter;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import static android.support.v4.app.NotificationCompat.DEFAULT_VIBRATE;
 
 public class HunterService extends Service {
     private IBinder mBinder = new LocalBinder();
@@ -25,6 +34,30 @@ public class HunterService extends Service {
         HunterService getService (){
             return HunterService.this;
         }
+    }
+
+    private void createLostGpsSignalNotification() {
+        NotificationCompat.Builder mBuilder;
+        mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.lost_gps_signal)
+                .setContentTitle(getString(R.string.notification_title_lost_gps_signal))
+                .setContentText(getString(R.string.notification_text_lost_gps_signal))
+                .setAutoCancel(true);
+
+        //Action when click on notification
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        int mId = 1;
+        mNotificationManager.notify(mId, mBuilder.build());
     }
 
 }
