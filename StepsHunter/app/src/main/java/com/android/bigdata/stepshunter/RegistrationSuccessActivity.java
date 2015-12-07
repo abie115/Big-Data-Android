17d8f46c3613b7c11bcaa0282b68852b1f76dd5b
nkopa <natalia16k@wp.pl>
2015-12-07 22:57:20 +0100

    #5 Wstepne widoki dla udanej i nieudanej rejestracji

---
+++ StepsHunter/app/src/main/java/com/android/bigdata/stepshunter/RegistrationSuccessActivity.java
@@ -0,0 +1,20 @@
+package com.android.bigdata.stepshunter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrationSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_success);
+    }
+
+    public void goToApplication(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
