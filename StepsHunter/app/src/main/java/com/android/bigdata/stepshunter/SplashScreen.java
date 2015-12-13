4056bd671e73cafbe7a31988ceed18d0afbffb94
Agnieszka <agnieszka-garsta@wp.pl>
2015-12-13 19:09:29 +0100

    Ekran powitalny jako layout / circular preloader #7

--- StepsHunter/app/src/main/java/com/android/bigdata/stepshunter/SplashScreen.java
+++ StepsHunter/app/src/main/java/com/android/bigdata/stepshunter/SplashScreen.java
@@ -4,22 +4,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
 
         Resources resources = getResources();
-        try {
-            Thread.sleep(resources.getInteger(R.integer.splash_screen_pause));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        Intent intent = new Intent(this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                startMainActivity();
+                finish();
+            }
+        }, resources.getInteger(R.integer.splash_screen_pause));
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
         startActivity(intent);
-        finish();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) startPreloaderAnimation();
+    }
+
+    public void startPreloaderAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animation_rotation);
+        findViewById(R.id.preloaderCircular).startAnimation(animation);
     }
 }
