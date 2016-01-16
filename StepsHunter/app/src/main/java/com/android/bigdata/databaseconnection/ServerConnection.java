dcad3bb612f5c46eced19d8b4d7370e900b8dd88
Agnieszka <agnieszka-garsta@wp.pl>
2016-01-16 02:05:37 +0100

    serverConnection poprawa sendPost()

--- StepsHunter/app/src/main/java/com/android/bigdata/databaseconnection/ServerConnection.java
+++ StepsHunter/app/src/main/java/com/android/bigdata/databaseconnection/ServerConnection.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Base64;
 import android.util.Log;
 
 import com.android.bigdata.stepshunter.R;
@@ -14,6 +15,8 @@ import com.android.bigdata.storagedata.InternalStorageFile;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -23,7 +26,7 @@ public class ServerConnection extends Application {
 
     Context context;
 
-    public ServerConnection(Context context){
+    public ServerConnection(Context context) {
         this.context = context;
     }
 
@@ -32,14 +35,14 @@ public class ServerConnection extends Application {
         public void onReceive(Context context, Intent intent) {
             if (isOnline()) {
                 Log.d("Test", "isOnline");
-                //threadSentPost();
+                threadSentPost();
                 context.unregisterReceiver(this);
             }
         }
     };
 
-    public void turnOnSendingCoordinates(){
-        new Thread(){
+    public void turnOnSendingCoordinates() {
+        new Thread() {
             @Override
             public void run() {
                 IntentFilter intentFilter = new IntentFilter();
@@ -48,7 +51,7 @@ public class ServerConnection extends Application {
 
                 try {
 
-                    while(true) {
+                    while (true) {
                         context.registerReceiver(broadcastReceiver, intentFilter);
                         sleep(context.getResources().getInteger(R.integer.frequency_of_sending_coordinates));
                     }
@@ -69,49 +72,84 @@ public class ServerConnection extends Application {
         return (networkInfo != null && networkInfo.isConnected());
     }
 
-    public void threadSentPost(){
-            new Thread() {
-                @Override
-                public void run() {
-                   sentPost();
-                }
-            }.start();
-        }
+    public void threadSentPost() {
+        new Thread() {
+            @Override
+            public void run() {
+                sentPost();
+            }
+        }.start();
+    }
 
     public void sentPost() {
-
         try {
-            String data = new InternalStorageFile(this).readJsonFromGpsFile().toString();
-            URL url = new URL(this.getString(R.string.server_url));
+            Log.d("Test", "start");
 
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            URL url = prepareServerUrl();
+            HttpURLConnection connection = prepareHttpUrlConnection(url);
+            String data = prepareDataToSent();
+            sentDataToServer(data, connection);
 
-            connection.setReadTimeout(5000);
-            connection.setConnectTimeout(10000);
-            connection.setRequestMethod("POST");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setRequestProperty("Accept", "application/json");
-            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            //<string name="server_url">http://test:test123@dziablo.ddns.net:3000/api/coordinates</string>
+            Log.d("Test", "server response " + connection.getResponseCode());
+            Log.d("Test", "server header " + connection.getHeaderFields().toString());
+            Log.d("Test", "server url " + url);
+            Log.d("Test", "server data " + data);
 
-            int HttpResult = connection.getResponseCode();
-            Log.d("Test", "server response " + HttpResult);
+            closeConnection(connection);
 
-            if(HttpResult == HttpURLConnection.HTTP_OK) {
-                OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-                out.write(data);
-                out.flush();
-                out.close();
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (MalformedURLException eURL) {
+            eURL.printStackTrace();
+            log(eURL);
+        } catch (IOException eIO) {
+            eIO.printStackTrace();
+            log(eIO);
         }
     }
 
-}
+    private void log(IOException e){
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        Log.d("Test", trace.toString());
+    }
 
+    private URL prepareServerUrl() throws MalformedURLException {
+        return new URL(
+                context.getString(R.string.server_protocol),
+                context.getString(R.string.server_host),
+                context.getResources().getInteger(R.integer.server_port),
+                context.getString(R.string.server_path));
+    }
+
+    private String prepareDataToSent() {
+        return new InternalStorageFile(context).readJsonFromGpsFile().toString();
+    }
+
+    private HttpURLConnection prepareHttpUrlConnection(URL url) throws IOException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setConnectTimeout(context.getResources().getInteger(R.integer.server_connect_timeout));
+        connection.setDoOutput(true);
+        connection.setChunkedStreamingMode(0);
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+        String user_info = context.getString(R.string.server_user_login) + ":" + context.getString(R.string.server_user_password);
+        String basic = Base64.encodeToString(user_info.getBytes(), Base64.NO_WRAP);
+        connection.setRequestProperty("Authorization", "Basic " + basic);
+
+        return connection;
+    }
+
+    private void sentDataToServer(String data, HttpURLConnection connection) throws IOException {
+        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+        out.write(data);
+        out.flush();
+        out.close();
+    }
+
+    private void closeConnection(HttpURLConnection connection){
+        connection.disconnect();
+    }
+}
